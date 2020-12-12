61ff21e626051eaf167ffe6a635d3f70a855644f
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2020-12-12 11:22:13 +0100

    added delay in between the tests

--- cypress/integration/shoppingCart.spec.js
+++ cypress/integration/shoppingCart.spec.js
@@ -1,18 +1,26 @@
 describe('E2E Test for shopping cart app', () => {
     it('Visits my Shopping cart app', () => {
+        cy.wait(2000);
         cy.visit('https://marni-shopping-cart.netlify.app/')
+        cy.wait(2000);
         // Find the link to login to app'
         cy.contains('here').click();
+        cy.wait(2000);
         // Add any item to the cart
         cy.contains('Add to Cart').click();
+        cy.wait(2000);
         // After adding item clicking on checkout
         cy.contains('Checkout').click();
+        cy.wait(2000);
         // Add few more items in checkout page
         cy.contains('1').click();
+        cy.wait(2000);
         // Add few more items in checkout page
         cy.contains('1').click();
+        cy.wait(2000);
         // Place order after adding items
         cy.contains('Place Order').click();
+        cy.wait(2000);
         // After placing order confirm and redirect to dashboard
         cy.contains('Place order').click();
     })
